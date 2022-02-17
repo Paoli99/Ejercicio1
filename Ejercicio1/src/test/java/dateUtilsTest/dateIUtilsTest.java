a9a3132d54b41a70755ce888574dd179bb463317
Paoli99 <31941327+Paoli99@users.noreply.github.com>
2022-02-17 16:45:12 -0400

    Update dateIUtilsTest.java
    updated dates to test

--- Ejercicio1/src/test/java/dateUtilsTest/dateIUtilsTest.java
+++ Ejercicio1/src/test/java/dateUtilsTest/dateIUtilsTest.java
@@ -9,35 +9,30 @@ public class dateIUtilsTest {
 
     @ParameterizedTest
     @CsvSource({
-            "4, mayo, 2020, dia incorrecto",
-            "5, mayo, 2020 , dia incorrecto",
-            "6, mayo, 2020, 6 mayo 2020",
-            "4, junio, 2020 , ingresar nuevos datos",
-            "5, junio, 2020 , ingresar nuevos datos",
-            "6, junio, 2020 , mes incorrecto",
-            "4, mayo, 2021 , ingresar nuevos datos",
-            "5, mayo, 2021 ,ingresar nuevos datos",
-            "6, mayo, 2021, gestion incorrecto",
+    //Datos correctos
+    "5,mayo,2020, 6 mayo 2020",
+    "28,febrero,2020, 29 febrero 2020",
+    "28,febrero,2021, 1 marzo 2021",
 
-            "27, febrero, 2020, mayo 2020, dia incorrecto",
-            "28, febrero, 2020,  mayo 2020 , dia incorrecto",
-            "29, febrero, 2020, 29 febrero 2020",
-            "27, enero, 2020 , ingresar nuevos datos",
-            "28, enero, 2020 , ingresar nuevos datos",
-            "29, enero, 2020 , mes incorrecto",
-            "27, febrero, 2021 , ingresar nuevos datos",
-            "28, febrero, 2021 ,ingresar nuevos datos",
-            "29, febrero, 2021, gestion incorrecto",
+    //Dias incorrectos
+    "-5,  mayo,  2020, dia incorrecto",
+    "0, febrero,  2021, dia incorrecto",
 
-            "27, febrero, 2021, dia incorrecto",
-            "28, febrero, 2021 , dia incorrecto",
-            "1, marzo, 2021, 1 marzo 2021",
-            "27, enero, 2021 , ingresar nuevos datos",
-            "28, enero, 2021 , ingresar nuevos datos",
-            "1, enero, 2021 , mes incorrecto",
-            "27, febrero, 2021, ingresar nuevos datos",
-            "28, febrero, 2021 ,ingresar nuevos datos",
-            "1, marzo, 2022, gestion incorrecto",
+    //Meses incorrectos
+    "5,  pasto,  2020, mes incorrecto",
+    "28, febreroro,  2020, mes incorrecto",
+    "28, fierro,  2021, mes incorrecto",
+
+
+    // Año incorrecto
+    "5,  mayo,  -2020, gestion incorrecto",
+    "28, febrero,  -1, gestion incorrecto",
+    "28, febrero,  -2021, gestion incorrecto",
+
+    // mas de un incorrecto
+    "-5,  mayo,  -2020, ingresar nuevos datos",
+    "-28, febreroro,  -1, ingresar nuevos datos",
+    "0, febrero,  -2021, ingresar nuevos datos",
 
     })
 
