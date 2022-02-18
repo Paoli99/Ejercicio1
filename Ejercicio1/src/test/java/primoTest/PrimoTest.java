fc13bf5be83504f1154fe8ee1dbb668687ae7c5c
Paoli99 <31941327+Paoli99@users.noreply.github.com>
2022-02-18 00:46:52 -0400

    Update PrimoTest.java

--- Ejercicio1/src/test/java/primoTest/PrimoTest.java
+++ Ejercicio1/src/test/java/primoTest/PrimoTest.java
@@ -7,13 +7,28 @@ import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 
 public class PrimoTest {
-
+    Primos servicioMock = Mockito.mock(Primos.class);
     @Test
     public void testEvenOddNumber(){
-        Primos meo = new Primos();
 
-        boolean expectedResults = true;
-        boolean actualResults = Primos.esPrimo(22);
-        Assertions.assertEquals(expectedResults, actualResults, "Error");
+        Mockito.when(servicioMock.esPrimo(8)).thenReturn(false);
+
+        Generador generador = new Generador(servicioMock);
+
+        String actualResult = generador.getNumberValues(8);
+        String expectedResult = "2 4 6";
+        Assertions.assertEquals(expectedResult, actualResult, "Error");
+
     }
+
+
+    /*@Test
+    public void testPrimo(){
+        Primos primos = new Primos();
+
+        boolean expectedResults = false;
+        boolean actualResults = Primos.esPrimo(11);
+        Assertions.assertEquals(expectedResults, actualResults, "error");
+    } */
+
 }
