61e351f11a09fab394e286250b330e8f1e4a902f
Paoli99 <31941327+Paoli99@users.noreply.github.com>
2022-02-18 00:25:11 -0400

    Update Primos.java

--- Ejercicio1/src/main/java/ejercicio2/Primos.java
+++ Ejercicio1/src/main/java/ejercicio2/Primos.java
@@ -4,18 +4,26 @@ public class Primos {
 
     public Primos(){}
 
-    public static boolean esPrimo(int numero){
-        if(numero >= 2) {
-            if (numero % 2 == 0) {
-                return true;
+    /*public int esPrimo(int numero) {
+        int x;
+        int a = 0;
+        for (int i = 2; i <= numero / 2; i++) {
+            x = numero % i;
+            if (x == 0) {
+                a=1; // no es primo
+            } else {
+                a=0; // es primo
             }
-            else{
+        }
+        return a;
+    } */
+
+    public static boolean esPrimo(int numero){
+        for (int i = 2; i < numero; i++)
+            if (numero % i == 0){
                 return false;
             }
-        }
 
-        else{
-            return false;
-        }
+        return true;
     }
 }
